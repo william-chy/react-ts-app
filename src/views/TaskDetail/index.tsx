f40033e2e119374ae65eb64005d1e8e1f724a3b5
william <26743487@qq.com>
2021-06-30 16:29:44 +0800

    feature:app initional

--- src/views/TaskDetail/index.tsx
+++ src/views/TaskDetail/index.tsx
@@ -4,8 +4,18 @@ import { Link } from 'react-router-dom';
 export default function Detail() {
   return (
     <React.Fragment>
-      <p>detail</p>
-      <Link to="/list">back</Link>;
+      <div className="panel">
+        <div className="panel-header">
+          <div className="panel-title">详情</div>
+        </div>
+        <div className="panel-nav" />
+        <div className="panel-body">
+          <p> 功能敬请期待</p>
+        </div>
+        <div className="panel-footer">
+          <Link to="/list">back</Link>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
--- src/views/TaskList/index.tsx
+++ src/views/TaskList/index.tsx
@@ -9,10 +9,10 @@ interface TaskItemProps {
 
 const TaskItem = (props: TaskItemProps) => {
   return (
-    <div className="task-item">
-      <input type="checkbox" name={'task' + props.id} id={'task' + props.id} />
-      <label htmlFor={'task' + props.id}>
-        <Link to={`/detail?id=${props.id}`}>{props.title}</Link>
+    <div className="menu-badge">
+      <label htmlFor={'task' + props.id} className="form-checkbox">
+        <input type="checkbox" name={'task' + props.id} id={'task' + props.id} />
+        <i className="form-icon" /> <Link to={`/detail?id=${props.id}`}>{props.title}</Link>
       </label>
     </div>
   );
@@ -24,14 +24,14 @@ interface TaskFormProps {
 const TaskForm = (props: TaskFormProps) => {
   const [ taskTitle, setTaskTitle ] = useState('');
   return (
-    <div>
+    <div className="input-group input-inline">
       <input
-        className="TaskInput"
+        className="form-input input-sm"
         type="text"
         value={taskTitle}
         onChange={(event) => setTaskTitle(event.target.value)}
       />
-      <button onClick={() => props.addFn(taskTitle)} className="TaskButton">
+      <button onClick={() => props.addFn(taskTitle)} className="btn btn-primary btn-sm input-group-btn">
         添加任务
       </button>
     </div>
@@ -47,13 +47,24 @@ const List = () => {
   }, []);
   const addTask = (task: string) => {
     taskList.push(task);
-    settaskList([...taskList]);
+    settaskList([ ...taskList ]);
   };
   return (
     <React.Fragment>
-      <h1>待办事项</h1>
-      <TaskForm addFn={addTask} />
-      {taskList.map((item, index) => <TaskItem key={index} title={item} id={index} />)}
+      <div className="panel">
+        <div className="panel-header">
+          <div className="panel-title">
+            <h1>待办事项</h1> <Link to="/">back</Link>
+          </div>
+        </div>
+        <div className="panel-nav" />
+        <div className="panel-body">
+          {taskList.map((item, index) => <TaskItem key={index} title={item} id={index} />)}
+        </div>
+        <div className="panel-footer">
+          <TaskForm addFn={addTask} />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
